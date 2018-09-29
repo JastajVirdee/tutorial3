d0f20c6b268e42562aedd3837ebe3488abf5b07b
JastajVirdee <jastaj.virdee@mail.mcgill.ca>
2018-09-29 13:01:48 -0400

    put local repo on guthub

---
+++ src/main/java/com/mcgill/ca/tutorial3/App.java
@@ -0,0 +1,11 @@
+package com.mcgill.ca.tutorial3;
+
+public class App 
+{
+    private int x;
+    public static void main( String[] args )
+    {
+	String s = "Hello!";
+        System.out.println(s.length());
+    }
+}
